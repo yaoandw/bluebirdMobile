af0e4ceff1f695c5e46ea9bac02327ac10901bf2
yaoandw <yaoandw@163.com>
2017-07-27 15:53:41 +0800

    use pure vue

--- app.js
+++ app.js
@@ -26,8 +26,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-//app.use(express.static(path.join(__dirname, 'vue')));
-app.use(express.static(path.join(__dirname, 'vonic')));
+app.use(express.static(path.join(__dirname, 'vue')));
+// app.use(express.static(path.join(__dirname, 'vonic')));
 
 app.use('/', index);
 app.use('/users', users);
