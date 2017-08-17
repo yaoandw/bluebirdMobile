7fc3283ee0d1c270dcb313f3aa0e4a67d24f0d96
yaoandw <yaoandw@163.com>
2017-08-17 10:53:20 +0800

    guide

---
+++ vue/static/blackbird.js
@@ -0,0 +1,11 @@
+/**
+ * Created by chenlinling on 8/17/17.
+ */
+window.onload = function () {
+  setTimeout(doOnLoad,100);
+};
+
+function doOnLoad() {
+  // alert(1);
+  window.scrollTo(0, 0);
+}
